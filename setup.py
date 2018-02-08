dc0d970485a529509a557dfe5b66710e3489b1b3
Tim Martin <tim@timmartin.me>
2018-02-07 22:28:57 -0500

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='flask-deprecate',
-    version='0.1.2',
+    version='0.1.3',
     description="Easy decorators for deprecating flask views and blueprints",
     long_description=readme + '\n\n' + history,
     author="Tim Martin",
